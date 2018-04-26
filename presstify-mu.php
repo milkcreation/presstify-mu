85dc87d4e28290435e814b47004c84c2f4f9651c
Jordy Manner <jordy.manner@milkcreation.fr>
2018-04-26 14:19:05 +0000

    trunk -- deploiement initial -- 1.1

--- presstify-mu.php
+++ presstify-mu.php
@@ -1,3 +1,13 @@
 <?php
 
-new \tiFy\tiFy();
+/**
+ * @name presstify/presstify-mu
+ * @desc Chargement de presstiFy en tant que mu-plugin.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package mu-plugin
+ * @version 1.1.0
+ */
+
+use tiFy\tiFy;
+
+new tiFy();
