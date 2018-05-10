6f990c108f74b331a844f431c7e97d21070ef45e
mastermilk <mastermilk@da7b893f-791f-4015-b6c2-6696fa8243f3>
2018-05-10 16:13:35 +0000

    branch2.0 -- compatibilité tiFy v2

---
+++ branches/2.0/presstify-mu.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @name presstify/presstify-mu
+ * @desc Chargement de presstiFy en tant que mu-plugin.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package mu-plugin
+ * @version 2.0.0
+ */
+
+new \tiFy\tiFy();
