27a3e73c8f2db15c4345226d2705055f24c05996
Jordy Manner <jordy.manner@milkcreation.fr>
2018-06-13 09:31:38 +0000

    presstify-mu -- trunk -- ajustement version tiFy

--- presstify-mu.php
+++ presstify-mu.php
@@ -5,7 +5,7 @@
  * @desc Chargement de presstiFy en tant que mu-plugin.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package mu-plugin
- * @version 2.0.0
+ * @version 1.1.0
  */
 
 new \tiFy\tiFy();
