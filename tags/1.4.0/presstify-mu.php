d7ab2e4bcc964c8b83824bd5936c49a8bce86a51
mastermilk <mastermilk@da7b893f-791f-4015-b6c2-6696fa8243f3>
2018-08-20 11:31:46 +0000

    unification n° version tiFY compatible 1.4

---
+++ tags/1.4.0/presstify-mu.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * @name presstify/presstify-mu
+ * @desc Chargement de presstiFy en tant que mu-plugin.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package mu-plugin
+ * @version 1.1.0
+ */
+
+new \tiFy\tiFy();
