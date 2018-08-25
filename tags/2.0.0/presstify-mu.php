1906f0e15de518e42c70d4b8966f4569ff99c31c
mastermilk <mastermilk@da7b893f-791f-4015-b6c2-6696fa8243f3>
2018-08-25 18:41:36 +0000

    tag 2.0.0 -- Réécriture

---
+++ tags/2.0.0/presstify-mu.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @name PresstiFy MuPlugin
+ * @desc Mu-plugin Wordpress de chargement du framework presstiFy.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package presstiFy
+ * @version 2.0.0
+ */
+
+use tiFy\tiFy;
+
+new tiFy();
