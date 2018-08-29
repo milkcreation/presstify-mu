b34f4fda140c4014b2325096b0aec9e06a8f5d22
mastermilk <mastermilk@da7b893f-791f-4015-b6c2-6696fa8243f3>
2018-08-29 08:14:16 +0000

    tag 1.4.1 -- Mise à jour mineure

---
+++ tags/1.4.1/presstify-mu.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @name PresstiFy MuPlugin
+ * @desc Mu-plugin Wordpress de chargement du framework presstiFy.
+ * @author Jordy Manner <jordy@milkcreation.fr>
+ * @package presstiFy
+ * @version 1.4.1
+ */
+
+use tiFy\tiFy;
+
+new tiFy();
