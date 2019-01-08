5f43dc8255d9ade12543c77367dc6774aae103cc
mastermilk <mastermilk@da7b893f-791f-4015-b6c2-6696fa8243f3>
2019-01-08 11:11:53 +0000

    branch 1.2 -- Déploiement initial

--- branches/1.2/presstify-mu.php
+++ branches/1.2/presstify-mu.php
@@ -5,7 +5,7 @@
  * @desc Mu-plugin Wordpress de chargement du framework presstiFy.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstiFy
- * @version 2.0.0
+ * @version 1.2.0
  */
 
 use tiFy\tiFy;
