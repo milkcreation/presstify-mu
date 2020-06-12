0a7168395a7ff3f51a61f89db341797c5324d4b4
Jordy Manner <jordy.manner@milkcreation.fr>
2020-06-12 15:29:38 +0000

    presstify-mu -- 2.0 -- évolution

--- presstify-mu.php
+++ presstify-mu.php
@@ -5,9 +5,9 @@
  * @desc Mu-plugin Wordpress de chargement du framework presstiFy.
  * @author Jordy Manner <jordy@milkcreation.fr>
  * @package presstiFy
- * @version 2.0.0
+ * @version 2.0.1
  */
 
 use tiFy\tiFy;
 
-new tiFy();
+(new tiFy())->boot();
